2c70d5029c43b427e6695101a4ba4dbf2ebedda4
root <root@ip-10-0-1-241.ec2.internal>
2023-11-08 06:56:34 +0000

    adding auth3

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,8 +3,10 @@ export type AmplifyDependentResourcesAttributes = {
     "amplifyreactgraphqlae22ab12": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
